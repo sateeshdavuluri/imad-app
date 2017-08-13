7cdc4ed576ace15f6c4e6f0581597d073f379b11
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-13 18:41:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,51 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var wedding = {
+    title : 'wedding | satD',
+    heading: 'Types',
+    content:` <p>There are many ways to count::</p>
+                `,
+};
 
+
+function createTemplate(data){ 
+
+var title = data.title;
+var heading = data.heading;
+var content = data.content;
+
+var htmlTemplate = `  
+    <html>
+        <head>
+            <title>${title}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="/ui/style.css" rel="stylesheet" />
+            
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <br/>
+                
+                <div>
+                   <h3> ${heading}
+                   </h3>
+                </div>
+
+                <div>
+                   ${content}
+                    
+                </div>
+            </div>
+        </body>
+    </html>
+`;
+
+return htmlTemplate;
+}
 
 
 
@@ -22,7 +66,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/wedding',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'wedding.html'));
+     res.send(createTemplate(wedding));
 });
 
 app.get('/pandit',function(req,res){
