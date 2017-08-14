6992e405c4e3ae0f0590e9b2ba48c072512a23c5
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-14 16:52:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,10 @@ app.get('/:eventsName',function(req,res){
      res.send(createTemplate(events[eventsName]));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 
 
 
