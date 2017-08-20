625447bc4ad91d920a0bdd0c945d9b856fdf323e
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-20 23:41:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,9 +46,23 @@ touch.onclick=function(){
   request.send(null);
 };
 
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 
-
-
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //make req to server and send name
+    
+    // capture and render the names as a list
+    var names =["sat1","sat2","sat3"];
+    var list ="";
+    
+    for(var i=0;i<names.length;i++){
+        list += '<li>' + names[i] +'</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
 
 
 
