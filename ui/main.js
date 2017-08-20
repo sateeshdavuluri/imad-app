49b0a79ec5ee701eb5aa537a41e1f2cb79cf48ef
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-20 17:31:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML="good Morning";
+
+var img= document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginleft = '100px';
+    
+};
