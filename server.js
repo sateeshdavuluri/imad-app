b4c3dbc84a59bbd7303a72ef8586a2ae19cfd732
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-20 18:47:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var counter =0;
-app.get('/counter', function(){
+app.get('/counter', function(req,res){
   counter = counter +1;
   res.send(counter.toString());
 });
