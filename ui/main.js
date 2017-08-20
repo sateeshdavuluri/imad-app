b0ee9ef605a2d519951f0a9a2219522948968c43
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-20 18:02:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ var img= document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft+ 10;
-    img.style.marginLeft = margiLeft +'px';
+    img.style.marginLeft = marginLeft +'px';
 }
 img.onclick = function() {
     var interval = setInterval(moveRight,50); //for every 50 ms moveRight func called
