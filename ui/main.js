7846758e8423ae9a046063504f27fbd56d74d352
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-20 18:02:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,20 @@
 console.log('Loaded!');
 
+// change main text
 var element = document.getElementById('main-text');
 
 element.innerHTML="good Morning";
 
+//move an element 
 var img= document.getElementById('madi');
+
 var marginLeft = 0;
 function moveRight(){
-    marginLeft += 10;
+    marginLeft = marginLeft+ 10;
     img.style.marginLeft = margiLeft +'px';
 }
 img.onclick = function() {
-    var interval = setInterval(moveRight,50);
+    var interval = setInterval(moveRight,50); //for every 50 ms moveRight func called
     // img.style.marginLeft = '100px';
+    //to move img its CSS been modified
 };
