1bf08608cf49a9e0fd8458e04c09123b84c15f81
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-22 00:36:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,9 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-
+app.get('/favicon.ico',function(req,res){
+    res.senFile(path.join(__dirname,'ui','favicon.ico'));
+});
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
