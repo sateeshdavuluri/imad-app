f5efec9b84cb7ba3dc808a65a0507203b26ac3be
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-22 00:42:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/favicon.ico',function(req,res){
+app.get('/ui/favicon.ico',function(req,res){
     res.senFile(path.join(__dirname,'ui','favicon.ico'));
 });
 
