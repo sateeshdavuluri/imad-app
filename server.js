4462fddd8485ae46024e9ad440ca63f56235c606
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-22 20:23:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/favicon.ico', function (req, res) {
 });
 
 
-var pool = new Pool(config)
+var pool = new Pool(config);
 app.get('/test-db', function(req,res){
    // make a sect request
    
