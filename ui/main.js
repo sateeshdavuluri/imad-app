d356cabcb356aac057defd82f73d9b1ce89616e7
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-30 16:51:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,8 +63,8 @@ submit.onclick = function(){
          if(request.status === 200){
             
             // capture and render the names as a list
-            var names = request.responseText;
-            names = JSON.parse('names');
+            var names = 'request.responseText';
+            names = JSON.parse(names);
             var list ="";
             
             if(names.length<=10){
