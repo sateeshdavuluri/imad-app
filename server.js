a9ca2379579f3bcd733a16a388cc4c4621523fd7
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-08-30 17:39:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/favicon.ico', function (req, res) {
 
 function hash(input,salt){
    //how to create a hash
-   var hashed = cryptp.pbkdf2Sync(input,salt,100000,512,'sha512');
+   var hashed = crypto.pbkdf2Sync(input,salt,100000,512,'sha512');
   
   return hashed.toString('hex');
     
