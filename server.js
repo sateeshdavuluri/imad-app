00a15d677af91de0a184aa590d90bd9661d79baf
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-09-08 01:58:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,6 +142,14 @@ app.get('/check-login', function(req,res){
     }
 });
 
+app.get('/logout',function(req,res){
+    
+   delete req.session.auth;
+   res.send('You are LoggedOuy;')
+    
+});
+
+
 
 
 //database pool connect
