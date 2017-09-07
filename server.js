5fda4fee6f3c0fe2e127bfd6153e4c07fe0d7093
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-09-08 02:00:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.get('/check-login', function(req,res){
 app.get('/logout',function(req,res){
     
    delete req.session.auth;
-   res.send('You are LoggedOuy;')
+   res.send('Logged Out');
     
 });
 
