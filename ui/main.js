6fd240cc17d97a2401c9bd18a589aec0748b3e6b
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-09-07 22:05:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -131,7 +131,7 @@ login.onclick = function(){
     console.log(username);  //for test/debugging only
     console.log(password);  //for test/debugging
     
-  request.open('POST', 'http://sateeshdavuluri.imad.hasura-app.io/login', true);
+  request.open("POST", 'http://sateeshdavuluri.imad.hasura-app.io/login', true);
   request.setRequestHeader('Content-Type','application/json');
   request.send(JSON.stringify({ username: username, password: password }));
   
