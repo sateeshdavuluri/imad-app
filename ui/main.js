4d3f83444d7e76cfb9d72432bbb3a9dea2bcf570
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-09-09 22:21:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -99,7 +99,7 @@ submit.onclick = function(){
 // Login username pswrd
 var login = document.getElementById('login_btn');
 
-login.onClick=function(){
+login.onclick=function(){
     //make req to server and send name
      //create req counter
    
