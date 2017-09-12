ee6781db161a68660a2c2fc9085e0b8dcf42e8b4
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-09-12 15:52:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -186,7 +186,9 @@ var events= {
     };
 
 
+
 //html template 
+
 function createTemplate(data){ 
 
 var title = data.title;
@@ -248,7 +250,7 @@ app.get('/events/:eventsName',function(req,res){
      // in sql query " - " appers in title then add single quotes to variable
      // sql injection  ';DELETE from "events" where 'a' = 'a 
      //to protect from sql injection pg librarysuggess to add "$1" for ' ' variable in array 
-     pool.query("SELECT * FROM events where title = $1",[req.params.eventsName],function(err,result){
+  /*   pool.query("SELECT * FROM events where title = $1",[req.params.eventsName],function(err,result){
          if(err){
              res.status(500).send(err.toString());
          } else {
@@ -260,7 +262,8 @@ app.get('/events/:eventsName',function(req,res){
              }
          }
      });
-     
+     */
+     res.send(createTemplate(events[eventsName] ));
 });
 
 app.get('/ui/main.js', function (req, res) {
