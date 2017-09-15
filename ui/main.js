048935bedc397444f6c3b4964a4a66d31cd958d2
sateeshdavuluri <30693297+sateeshdavuluri@users.noreply.github.com>
2017-09-15 23:45:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ touch.onclick=function(){
    //complete the response and store it in a varible
    request.onreadystatechange= function(){
        
-       if(request.readystate === XMLHttpRequest.DONE){
+       if(request.readystate === XMLHttpRequest.Done){
          if(request.status === 200){
              var counter = request.responseText;
              var span = document.getElementById('count');
@@ -57,7 +57,7 @@ submit.onclick = function(){
    //complete the response and store it in a varible
    request.onreadystatechange = function(){
        
-       if(request.readystate === XMLHttpRequest.DONE){
+       if(request.readystate === XMLHttpRequest.Done){
          if(request.status === 200){
             
             // capture and render the names as a list
@@ -112,7 +112,7 @@ login.onclick=function(){
    //complete the response and store it in a varible
    request.onreadystatechange= function(){
        
-       if(request.readystate === XMLHttpRequest.DONE){
+       if(request.readystate === XMLHttpRequest.Done){
          if(request.status === 200){
             console.log("user logged-in");
             alert("Logged-in Successfully!");
@@ -131,7 +131,8 @@ login.onclick=function(){
     
         }
       };
-
+        username.value = " ";
+        password.value="";
     
     //make req 
   var username = document.getElementById('username').value;
@@ -161,7 +162,7 @@ logout.onclick = function(){
    //complete the response and store it in a varible
    request.onreadystatechange= function(){
        
-       if(request.readystate === XMLHttpRequest.DONE){
+       if(request.readystate === XMLHttpRequest.Done){
          if(request.status === 200||request.status === 304){
             console.log("user logged-out");
             alert("Logged-out Successfully!");
@@ -197,7 +198,7 @@ register.onclick=function(){
    //complete the response and store it in a varible
    request.onreadystatechange= function(){
        
-       if(request.readystate === XMLHttpRequest.DONE){
+       if(request.readystate === XMLHttpRequest.Done){
              
              if(request.status === 200){
                 console.log("user Registered");
